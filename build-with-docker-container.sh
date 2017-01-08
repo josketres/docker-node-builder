83fc6bce40581173e9dd443f38ffadba2233d514
Josue Zarzosa <josketres@gmail.com>
2017-01-08 12:47:00 +0100

    Add travis CI

--- build-with-docker-container.sh
+++ build-with-docker-container.sh
@@ -6,14 +6,15 @@ mount=$(pwd):/project
 
 set -e 
 
-# cleanup
+# avoid conflicts with node_modules folder on the host
+# and link the cached node_modules from the image
 docker run --rm -v $mount $image sh -c 'cd /project && mv node_modules host_node_modules >/dev/null 2>&1 || true && ln -s /cached-dependencies/node_modules node_modules' 
 
 # build
 docker run --rm -v $mount $image sh -c 'cd /project && gulp'
 
-# interactive console
+# interactive console for debugging
 #docker run --rm -v $mount -it $image sh -c 'cd /project && bash' 
 
-# cleanup
+# cleanup links and restore host node_modules (if it exists)
 docker run --rm -v $mount $image sh -c 'cd /project && rm node_modules && mv host_node_modules node_modules >/dev/null 2>&1 2>&1 || true' 
