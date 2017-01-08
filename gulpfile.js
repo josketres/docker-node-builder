7df405d52f3a28a57e05ad7f1bb82d468c6abbce
Josue Zarzosa <josketres@gmail.com>
2017-01-08 12:31:54 +0100

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@ var sass = require('gulp-sass');
 
 gulp.task('default', function() {
       return gulp.src('./src/sass/**/*.scss')
-                                  .pipe(sass().on('error', sass.logError))
-                                  .pipe(gulp.dest('./dist/css'));
-
+            .pipe(sass().on('error', sass.logError))
+            .pipe(gulp.dest('./dist/css'));
 });
